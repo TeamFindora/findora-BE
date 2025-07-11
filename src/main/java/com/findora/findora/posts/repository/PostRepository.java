8c4f70b3d84ff14fa3620c70d06d522e43619d73
Ol0Ju <ejwj9900@naver.com>
2025-07-11 13:28:25 +0900

    카테고리 게시판 기능구현

--- src/main/java/com/findora/findora/posts/repository/PostRepository.java
+++ src/main/java/com/findora/findora/posts/repository/PostRepository.java
@@ -1,6 +1,11 @@
 package com.findora.findora.posts.repository;
 
+import com.findora.findora.categories.model.Category;
 import com.findora.findora.posts.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long>{
+    List<Post> findByCategory(Category category);
 }
