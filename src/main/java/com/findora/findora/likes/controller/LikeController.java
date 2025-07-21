70a85edf06e7c8e6b44e7af7a050228e9ed3bb4e
Ol0Ju <ejwj9900@naver.com>
2025-07-21 14:39:13 +0900

    controller 수정

--- src/main/java/com/findora/findora/likes/controller/LikeController.java
+++ src/main/java/com/findora/findora/likes/controller/LikeController.java
@@ -2,10 +2,12 @@ package com.findora.findora.likes.controller;
 
 import com.findora.findora.likes.dto.LikeResponseDto;
 import com.findora.findora.likes.service.LikeService;
+import com.findora.findora.users.service.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,41 +20,42 @@ public class LikeController {
 
     // user의 게시글 좋아요 상태
     @GetMapping("/status")
-    @Operation(summary = "게시글 좋아요 상태 조회")
+    @Operation(summary = "게시글 user의 좋아요 상태 ")
     public ResponseEntity<LikeResponseDto> getPostLikeStatus(
             @PathVariable Long postId,
+            @AuthenticationPrincipal CustomUserDetails user,
             @RequestParam(required = false) Long userId) {
-        LikeResponseDto response = likeService.getPostLikeStatus(postId, userId);
+        LikeResponseDto response = likeService.getPostLikeStatus(postId, user.getId());
         return ResponseEntity.ok(response);
     }
 
     // 게시글 좋아요 누르기/취소
     @PostMapping("/toggle")
-    @Operation(summary = "게시글 좋아요 토글 (좋아요 누르기/취소)")
+    @Operation(summary = "게시글 좋아요 누르기/취소")
     public ResponseEntity<LikeResponseDto> togglePostLike(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal CustomUserDetails user,
             @PathVariable Long postId) {
-        LikeResponseDto response = likeService.togglePostLike(userId, postId);
+        LikeResponseDto response = likeService.togglePostLike(user.getId(), postId);
         return ResponseEntity.ok(response);
     }
 
     // user의 댓글 좋아요 상태
     @GetMapping("/comments/{commentId}/status")
-    @Operation(summary = "댓글 좋아요 상태 조회")
+    @Operation(summary = "댓글 user의 좋아요 상태")
     public ResponseEntity<LikeResponseDto> getCommentLikeStatus(
             @PathVariable Long commentId,
-            @RequestParam(required = false) Long userId) {
-        LikeResponseDto response = likeService.getCommentLikeStatus(commentId, userId);
+            @AuthenticationPrincipal CustomUserDetails user) {
+        LikeResponseDto response = likeService.getCommentLikeStatus(commentId, user.getId());
         return ResponseEntity.ok(response);
     }
 
     // 댓글 좋아요 좋아요 누르기/취소
     @PostMapping("/comments/{commentId}/toggle")
-    @Operation(summary = "댓글 좋아요 토글 (좋아요 누르기/취소)")
+    @Operation(summary = "댓글 좋아요 누르기/취소")
     public ResponseEntity<LikeResponseDto> toggleCommentLike(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal CustomUserDetails user,
             @PathVariable Long commentId) {
-        LikeResponseDto response = likeService.toggleCommentLike(userId, commentId);
+        LikeResponseDto response = likeService.toggleCommentLike(user.getId(), commentId);
         return ResponseEntity.ok(response);
     }
 
